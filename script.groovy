d8937bddd9700989e4020f611794295b58ff0caf
mojoe277 <122628203+mojoe277@users.noreply.github.com>
2023-02-25 01:09:13 +0200

    last1

--- script.groovy
+++ script.groovy
@@ -1,15 +1,6 @@
-def test() {
-    echo "Testing the application..."
-    echo "Executing pipeline for branch $BRANCH_NAME"
-} 
-
-def build() {
-    echo "Building the application..."
- 
-} 
-
 def deployApp() {
-    echo 'Deploying the application...'
-} 
+    echo 'deploying the application...'
+}
 
 return this
+
